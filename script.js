9cf7d0a242700f65addc31aba5124e16cee07269
Minhaz Halim Zim <31536403+minhazhalim@users.noreply.github.com>
2024-08-04 00:37:27 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuButton = document.querySelectorAll('.menu-button');
 const screenOverlay = document.querySelector('.main-layout .screen-overlay');
-const themeButton = document.querySelector('.navbar .theme-button .uil');
+const uil = document.querySelector('.navbar .theme-button .uil');
 menuButton.forEach(button => {
      button.addEventListener('click',() => {
           document.body.classList.toggle('sidebar-hidden');
@@ -11,15 +11,15 @@ screenOverlay.addEventListener('click',() => {
 });
 if(localStorage.getItem('darkMode') === 'enabled'){
      document.body.classList.add('dark-mode');
-     themeButton.classList.replace('uil-moon','uil-sun');
+     uil.classList.replace('uil-moon','uil-sun');
 }else{
-     themeButton.classList.replace('uil-sun','uil-moon');
+     uil.classList.replace('uil-sun','uil-moon');
 }
-themeButton.addEventListener('click',() => {
+uil.addEventListener('click',() => {
      const isDarkMode = document.body.classList.toggle('dark-mode');
      localStorage.setItem('darkMode',isDarkMode ? 'enabled' : 'disabled');
-     themeButton.classList.toggle('uil-sun',isDarkMode);
-     themeButton.classList.toggle('uil-moon',!isDarkMode);
+     uil.classList.toggle('uil-sun',isDarkMode);
+     uil.classList.toggle('uil-moon',!isDarkMode);
 });
 if(window.innerWidth >= 768){
      document.body.classList.remove('sidebar-hidden');
